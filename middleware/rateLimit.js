51915f0d35c229e50f7182dc31a3c8af1e33ff04
Anizoomy <habeebolayemi518@gmail.com>
2026-02-07 10:30:40 +0100

    login and signup limiter

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,5 +1,5 @@
-const {rateLimit} = require('express-rate-limit');
-const {RedisStore} = require('rate-limit-redis');
+const { rateLimit } = require('express-rate-limit');
+const { RedisStore } = require('rate-limit-redis');
 const redisClient = require('../utils/redis');
 
 exports.loginLimiter = rateLimit({
@@ -10,7 +10,7 @@ exports.loginLimiter = rateLimit({
     }),
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 5, // limit each IP to 5 requests per windowMs
-    message: 'Too many login attempts from this IP, please try again after 15 minutes',
+    message: { message: 'Too many login attempts from this IP, please try again after 15 minutes' },
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
@@ -22,7 +22,7 @@ exports.apiLimiter = rateLimit({
     }),
     windowMs: 60 * 1000, // 1 minute
     max: 60, // limit each IP to 60 requests per windowMs
-    message: 'Too many requests from this IP, please try again after a minute',
+    message: { message: 'Too many requests from this IP, please try again after a minute' },
     standardHeaders: true,
     legacyHeaders: false
 });
